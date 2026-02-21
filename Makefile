0c2d55ec7ac9b09de2480173584ff816ca0f075c
cof <cof@fastmail.fm>
2026-02-21 20:50:28 +0000

    Added robust socket read_line + simple server tests

--- Makefile
+++ Makefile
@@ -1,14 +1,29 @@
 # Makefile
 CC = gcc
-CFLAGS = -Wall -O2
+
+CFLAGS = -Wall -Wextra -O2
+ifdef DEBUG 
+	CFLAGS += -O0 -g
+endif
 LDFLAGS = -static
 
-.PHONY: all clean
+.PHONY: all clean test
+
 all: server
 
 server: server.o
+
 clean:
 	rm -f *.o server
+
+test: server
+	@echo "Starting tests"; \
+	./server & SERVER_PID=$$!; \
+    timeout 3 bash -c 'until nc -z localhost 6379; do sleep 0.1; done'; \
+	echo "SET foo bar" | nc -w 1 -N localhost 6379 | grep -q "SET foo bar"; \
+	echo "GET foo" | nc -w 1 -N localhost 6379 | grep -q "GET foo"; \
+	echo "DEL foo" | nc -w 1 -N localhost 6379 | grep -q "DEL foo"; \
+	kill $$SERVER_PID
 
 %.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
--- server.c
+++ server.c
@@ -5,8 +5,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define TCP_PORT 6379
+#define ERR_READSOCK -1
+#define ERR_CLOSESOCK -2
+#define ERR_READLINE -3
+
 
 int setup_listener(void)
 {
@@ -64,41 +69,117 @@ void process_line(const char *line)
     puts(line);
 }
 
-int handle_client(int fd)
+
+struct strvec {
+    char *base;
+    size_t len;
+};
+
+struct rwbuf {
+    size_t cap; // fixed size 
+    size_t len; // bytes avail to read
+    char *rptr;  // start of bytes to read
+    char data[4096];
+};
+
+void init_rwbuf(struct rwbuf *buf)
 {
-    char buf[MAX_LINE];
-    char *pos = buf;
-    int rem = sizeof(buf);
+    buf->cap = sizeof(buf->data);
+    buf->len = 0;
+    buf->rptr = buf->data;
+}
+
+int read_sock(int fd, struct rwbuf *buf)
+{
+    int tr = 0;
+    char *rptr = buf->rptr + buf->len;
+    size_t rem = buf->data + buf->cap - rptr;
 
     while (1) {
-        ssize_t nr = read(fd, pos, rem);
-        if (nr <= 0) {
-            close(fd);
-            return -1;
+        
+        // read as much as we can
+        ssize_t nr = read(fd, rptr, rem);
+
+        if (nr == -1)  {
+            // read failed
+           if (errno == EINTR) continue; // interrupt ?
+           if (errno != EAGAIN) tr = ERR_READSOCK;
+           break;
         }
-        pos += nr;
+        if (nr == 0) {
+            // closed ?
+            if (tr == 0) tr = ERR_CLOSESOCK;
+            break;
+        }
+
+        tr += nr;
+
+        rptr += nr;
         rem -= nr;
-        char *eol = memchr(buf, '\n', pos - buf);
-        if (!eol) {
-            if (rem <= 0) {
-                perror("readline too big");
-                close(fd);
-                return -1;
-            }
-        }
-        else {
-            *eol = '\0';
-            process_line(buf);
-            // deal with partial line
-            eol++;
-            int len = pos - eol;
-            memmove(buf, eol, len);
-            pos = buf + len;
-            rem = sizeof(buf) - len;
-        }
+        buf->len += nr;
+
+        // socket has less 
+        break;
+
     }
 
-    return fd;
+    // total read or errcode
+    return tr;
+}
+
+int read_line(struct rwbuf *buf, struct strvec *line)
+{
+    char *eol = memchr(buf->rptr, '\n', buf->len);
+    if (eol) {
+        // found line
+        size_t len = eol - buf->rptr + 1;
+        char *str = buf->rptr;
+        // remove from buffer
+        buf->len -= len ;
+        buf->rptr += len;
+        // chop cr/lf
+        if (len && str[len - 1] == '\n') len--;
+        if (len && str[len - 1] == '\r') len--;
+        str[len] = '\0';
+        // store line
+        line->base = str;
+        line->len = len;
+        // have line or error
+        return len > MAX_LINE ? ERR_READLINE : (int) len;
+    }
+
+
+    // incomplete line
+    if (buf->len >= MAX_LINE) return ERR_READLINE;
+
+    if (buf->rptr > buf->data) {
+        // ensure partial line at buffer start
+        memmove(buf->data, buf->rptr, buf->len);
+        buf->rptr = buf->data;
+    }
+
+    return 0;
+}
+
+
+void handle_client(int fd)
+{
+    struct rwbuf buf;
+    struct strvec line;
+    int rc;
+
+    init_rwbuf(&buf);
+
+    while (1) {
+        rc = read_sock(fd, &buf);
+        if (rc < 0) break;
+        while ((rc = read_line(&buf, &line)) > 0) {
+            process_line(line.base);
+        }
+        if (rc < 0) break;
+    }
+
+    close(fd);
 }
 
 int main(int argc, char *argv[])
