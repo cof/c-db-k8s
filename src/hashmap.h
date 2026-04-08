c6b06cebc3e0ae796c0e3d74a8d7bf0f1f122335
cof <cof@fastmail.fm>
2026-04-08 22:45:13 +0100

    Added hashmap api

---
+++ src/hashmap.h
@@ -0,0 +1,178 @@
+/*
+ * MAP api
+ * -------
+ * hasmap32/hashmap64        : map types
+ * -
+ * map_init(m, capacity)  : initialize map
+ * map_free(m)            : free map memory
+ * map_attach(m, buffer, capacity) : attach static buffer to map
+ * -
+ * map_end(m)             : return capacity
+ * map_size(m)            : return elements in map
+ * -
+ * map_put(m, key, val)   : insert/update key,value entry
+ * map_get(m, key)        : find key returns map_end() if not found
+ * map_del(m, key)        : delete key returns map_end() if not found
+ * -
+ * map_key(m, index)      : get key at index
+ * map_val(m, index)      : get value at index
+ * map_rem(m, index)      : remove entry at index
+ * map_set(m, index, val) : update value at index
+ */
+#ifndef _HASHMAP_H_
+#define _HASHMAP_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+
+static inline uint32_t map_hash32(uint32_t key, int bits) 
+{
+    key ^= key >> 16;
+    return (key * 2654435761U) >> (32 - bits);
+}
+
+static inline uint32_t map_hash64(uint64_t key, int bits)
+{
+    key ^= key >> 33;
+    return (uint32_t)((key * 0x9E3779B97F4A7C15ULL) >> (64 - bits));
+}
+
+static inline uint32_t map_hashstr(uint64_t key, int bits)
+{
+    char *str = (char *) (uintptr_t) key;
+    uint64_t hash = dbj2a_hash_str(str);
+    return (hash * 0x9E3779B97F4A7C15ULL) >> (64 - bits);
+}
+
+static inline int map_streq(uint64_t a, uint64_t b) 
+{
+    if (a == b) return 1;
+    if (!a || !b) return 0;
+    return strcmp((char *)(uintptr_t)a, (char *)(uintptr_t)b) == 0;
+}
+
+// macros to add prefix to function names
+#define CAT_INNER(a, b) a##_##b
+#define CAT(a, b) CAT_INNER(a, b)
+#define MAP_FN(name) CAT(MAP_PREFIX, name)
+
+// -- 32-bit --
+#define MAP_TYPE   hashmap32
+#define MAP_PREFIX map32
+#define KEY_HASH   map_hash32
+#define KEY_EQ(a,b) ((a) == (b))
+#define KEY_TYPE   uint32_t
+#define VAL_TYPE   uint32_t
+#include "hashmap_impl.h"
+#undef MAP_TYPE
+#undef MAP_PREFIX
+#undef KEY_HASH
+#undef KEY_EQ
+#undef KEY_TYPE
+#undef VAL_TYPE
+
+// -- 64-bit --
+#define MAP_TYPE   hashmap64
+#define MAP_PREFIX map64
+#define KEY_HASH   map_hash64
+#define KEY_EQ(a,b) ((a) == (b))
+#define KEY_TYPE   uint64_t
+#define VAL_TYPE   uint64_t
+#include "hashmap_impl.h"
+#undef MAP_TYPE
+#undef MAP_PREFIX
+#undef KEY_HASH
+#undef KEY_EQ
+#undef KEY_TYPE
+#undef VAL_TYPE
+
+// -- str --
+#define MAP_TYPE   hashmapstr
+#define MAP_PREFIX mapstr
+#define KEY_HASH   map_hashstr
+#define KEY_EQ     map_streq
+#define KEY_TYPE   uint64_t
+#define VAL_TYPE   uint64_t
+#include "hashmap_impl.h"
+#undef MAP_TYPE
+#undef MAP_PREFIX
+#undef KEY_HASH
+#undef KEY_EQ
+#undef KEY_TYPE
+#undef VAL_TYPE
+
+// -- map api -- using generic
+
+#define map_init(M, C) _Generic((M), \
+    hashmap32*:  map32_init, \
+    hashmap64*:  map64_init, \
+    hashmapstr*: mapstr_init \
+)(M, C)
+
+#define map_free(M) _Generic((M), \
+    hashmap32*:  map32_free, \
+    hashmap64*:  map64_free, \
+    hashmapstr*: mapstr_free \
+)(M)
+
+#define map_attach(M, B, C) _Generic((M), \
+    hashmap32*:  map32_attach, \
+    hashmap64*:  map64_attach, \
+    hashmapstr*: mapstr_attach \
+)(M, B, C)
+
+#define map_end(M) _Generic((M), \
+    hashmap32*:  map32_end, \
+    hashmap64*:  map64_end, \
+    hashmapstr*: mapstr_end \
+)(M)
+
+#define map_size(M) _Generic((M), \
+    hashmap32*:  map32_size,  \
+    hashmap64*:  map64_size,  \
+    hashmapstr*: mapstr_size \
+)(M)
+
+#define map_put(M, K, V) _Generic((M), \
+    hashmap32*:  map32_put, \
+    hashmap64*:  map64_put, \
+    hashmapstr*: mapstr_put \
+)(M, K, V)
+
+#define map_get(M, K) _Generic((M), \
+    hashmap32*:  map32_get, \
+    hashmap64*:  map64_get, \
+    hashmapstr*: mapstr_get \
+)(M, K)
+
+#define map_del(M, K) _Generic((M), \
+    hashmap32*:  map32_del, \
+    hashmap64*:  map64_del, \
+    hashmapstr*: mapstr_del \
+)(M, K)
+
+#define map_key(M, I) _Generic((M), \
+    hashmap32*:  map32_key, \
+    hashmap64*:  map64_key, \
+    hashmapstr*: mapstr_key \
+)(M, I)
+
+#define map_val(M, I) _Generic((M), \
+    hashmap32*:  map32_val, \
+    hashmap64*:  map64_val, \
+    hashmapstr*: mapstr_val \
+)(M, I)
+
+#define map_rem(M, I) _Generic((M), \
+    hashmap32*:  map32_rem, \
+    hashmap64*:  map64_rem, \
+    hashmapstr*: mapstr_rem \
+)(M, I)
+
+#define map_set(M, I, V) _Generic((M), \
+    hashmap32*:  map32_set, \
+    hashmap64*:  map64_set, \
+    hashmapstr*: mapstr_set \
+)(M, I, V)
+
+#endif
---
+++ src/hashmap_impl.h
@@ -0,0 +1,201 @@
+// X-macro hash map - XXX dont add include guards
+
+typedef struct MAP_FN(entry) {
+    KEY_TYPE key;
+    VAL_TYPE val;
+} MAP_FN(entry);
+
+typedef struct MAP_TYPE {
+    MAP_FN(entry) *buckets;
+    uint32_t capacity;
+    uint32_t threshold;
+    uint32_t size;
+    uint32_t mask;
+    uint32_t nbits;
+    unsigned int is_fixed : 1;
+} MAP_TYPE;
+
+
+static inline KEY_TYPE MAP_FN(sanitize)(KEY_TYPE k) 
+{
+    return k == 0 ? (KEY_TYPE) -1 : k;
+}
+
+static inline uint32_t MAP_FN(calc_bits)(uint32_t n)
+{
+    return n ? 32 - __builtin_clz(n) : 0;
+}
+
+// resize map to new capacity
+static inline uint32_t MAP_FN(resize)(MAP_TYPE *m, uint32_t capacity) 
+{
+    if (m->is_fixed) return 0;
+
+    // calc new capacity
+	uint32_t nbits = MAP_FN(calc_bits)(capacity - 1);
+	if (nbits <  2) nbits = 2;
+    capacity = 1 << nbits;
+
+    // alloc memory
+    MAP_FN(entry) *buckets = calloc(capacity, sizeof(*buckets));
+    if (!buckets) return 0;
+
+    // rehash keys
+    uint32_t size = 0;
+    uint32_t mask = capacity - 1;
+    for (uint32_t i = 0; i < m->capacity; i++) {
+        KEY_TYPE key = m->buckets[i].key;
+        if (key) {
+            VAL_TYPE val = m->buckets[i].val;
+            uint32_t idx = KEY_HASH(key, nbits);
+            while (buckets[idx].key) {
+                idx = (idx + 1) & mask;
+            }
+            buckets[idx] = (MAP_FN(entry)) { key, val };
+            size++;
+        }
+    }
+
+    if (m->buckets) free(m->buckets);
+    m->buckets  = buckets;
+    m->capacity = capacity;
+    m->threshold = (capacity >> 1) + (capacity >> 2); 
+    m->size     = size;
+    m->mask     = mask;
+    m->nbits    = nbits;
+
+    // all done
+    return 1;
+}
+
+// initialize map
+static inline uint32_t MAP_FN(init)(MAP_TYPE *m, uint32_t capacity)
+{
+    return MAP_FN(resize)(m, capacity);
+}
+
+// free map memory
+static inline void MAP_FN(free)(MAP_TYPE *m)
+{
+    if (m->buckets && !m->is_fixed) {
+        free(m->buckets);
+        m->buckets = NULL;
+    }
+}
+
+static inline void MAP_FN(attach)(MAP_TYPE *m, MAP_FN(entry) *buffer, uint32_t capacity)
+{
+    m->buckets = buffer;
+    m->capacity = capacity;
+    m->threshold =  (capacity >> 1) + (capacity >> 2);
+    m->size = 0;
+    m->mask  = m->capacity -1;
+    m->nbits = MAP_FN(calc_bits)(capacity - 1);
+    m->is_fixed = 1;
+}
+
+// return map end - aka capacity
+static inline uint32_t MAP_FN(end)(MAP_TYPE *m)
+{
+    return m->capacity;
+}
+
+// return elements in map 
+static inline uint32_t MAP_FN(size)(MAP_TYPE *m)
+{
+    return m->size;
+}
+
+// put key, value in map - return index or end
+static inline uint32_t MAP_FN(put)(MAP_TYPE *m, KEY_TYPE key, VAL_TYPE val) 
+{
+    if (m->size >= m->threshold) {
+        if (!MAP_FN(resize)(m, m->capacity + 1)) return m->capacity;
+    }
+
+    if (key == (KEY_TYPE) -1) return m->capacity;
+    key = MAP_FN(sanitize)(key);
+    uint32_t idx = KEY_HASH(key, m->nbits);
+    uint32_t end = idx;
+
+    while (m->buckets[idx].key != 0) {
+        if (KEY_EQ(m->buckets[idx].key, key)) break;
+        idx = (idx + 1) & m->mask;
+        if (idx == end) return m->capacity;
+    }
+
+    if (!m->buckets[idx].key) m->size++;
+    m->buckets[idx] = (MAP_FN(entry)) { key, val };
+
+    return idx;
+}
+
+// get key in map - return index or end
+static inline uint32_t MAP_FN(get)(MAP_TYPE *m, KEY_TYPE key)
+{
+    if (m->size == 0) return m->capacity;
+
+    key = MAP_FN(sanitize)(key);
+    uint32_t idx = KEY_HASH(key, m->nbits);
+    uint32_t end = idx;
+
+    while (m->buckets[idx].key != 0) {
+        if (KEY_EQ(m->buckets[idx].key, key)) break;
+        idx = (idx + 1) & m->mask;
+        if (idx == end) return m->capacity;
+    }
+
+    return m->buckets[idx].key ? idx : m->capacity;
+}
+
+// remove index entry from map - uses "Algorithm R" aka backshifting
+static inline uint32_t MAP_FN(rem)(MAP_TYPE *m, uint32_t idx)
+{
+    if (m->size == 0 || idx >= m->capacity) return m->capacity;
+    uint32_t nidx = idx;
+
+    while (1) {
+        nidx = (nidx + 1) & m->mask;
+        if (nidx == idx || m->buckets[nidx].key == 0) break;
+        uint32_t kidx = KEY_HASH(m->buckets[nidx].key, m->nbits); 
+        if ((nidx < idx) ^ (kidx <= idx) ^ (kidx > nidx)) {
+            m->buckets[idx] = m->buckets[nidx];
+            idx = nidx;
+        }
+    }
+
+    m->buckets[idx].key = 0;
+    m->size--;
+
+    return idx;
+}
+
+// delete key from map - return index or end
+static inline uint32_t MAP_FN(del)(MAP_TYPE *m, KEY_TYPE key)
+{
+    key = MAP_FN(sanitize)(key);
+    uint32_t idx = MAP_FN(get)(m, key);
+    return idx == m->capacity ? idx : MAP_FN(rem)(m, idx);
+}
+
+// return key at index
+static inline KEY_TYPE MAP_FN(key)(MAP_TYPE *m, uint32_t idx)
+{
+    if (idx >= m->capacity || !m->buckets[idx].key) return 0;
+    return m->buckets[idx].key;
+}
+
+// return value at index
+static inline VAL_TYPE MAP_FN(val)(MAP_TYPE *m, uint32_t idx)
+{
+    if (idx >= m->capacity || !m->buckets[idx].key) return 0;
+    return m->buckets[idx].val;
+}
+
+// update value at index
+static inline uint32_t MAP_FN(set)(MAP_TYPE *m, uint32_t idx, VAL_TYPE val)
+{
+    if (idx >= m->capacity || !m->buckets[idx].key) return m->capacity;
+    m->buckets[idx].val = val;
+    return idx;
+}
